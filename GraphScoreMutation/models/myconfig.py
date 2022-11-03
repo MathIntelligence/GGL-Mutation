5cb4796d8f9cefa54a35bd9909ccc1b4dce7bd38
NguyenLabUKY <ducnguyen@uky.edu>
2022-11-03 15:47:35 -0400

    upload new file

---
+++ GraphScoreMutation/models/myconfig.py
@@ -0,0 +1,8 @@
+def mypath():
+    path = {}
+    path['JACKALDIR'] = "/home/ddng223/RI-Score-Mutation/software/jackal_64bit/bin/jackal.dir"
+    path['profix'] = '/home/ddng223/RI-Score-Mutation/software/jackal_64bit/bin/profix'
+    path['scap'] = '/home/ddng223/RI-Score-Mutation/software/jackal_64bit/bin/scap'
+    path['vmd'] = '/home/ddng223/Software/local/bin/vmd'
+    
+    return path
